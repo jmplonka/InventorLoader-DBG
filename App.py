231f28a59c3cae4a48e83480e1331a5316b75663
Jens M. Plonka <jens.plonka@de.bosch.com>
2019-01-11 09:54:49 +0100

    fixed messed up merge.

--- App.py
+++ App.py
@@ -24,15 +24,10 @@ class ViewObject():
 		self.ShapeColor   = None
 		self.Transparency = None
 		self.ShapeMaterial = ShapeMaterial()
-		self.Object       = ViewProvider()
 	def hide(self):
 		pass
 	def show(self):
 		pass
-
-class ViewProvider():
-	def __init__(self):
-		return
 
 class DocumentObjectGroup():
 	def __init__(self, name = ''):
